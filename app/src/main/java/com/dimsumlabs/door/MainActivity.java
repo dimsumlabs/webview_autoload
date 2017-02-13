96c693dec5f3755309133e1c9e483555dfdfd5b8
Benedict Lau <ben.hy.lau@gmail.com>
2017-02-13 19:54:24 +0800

    Add wakelock and read URL from storage

--- app/src/main/java/com/dimsumlabs/door/MainActivity.java
+++ app/src/main/java/com/dimsumlabs/door/MainActivity.java
@@ -1,11 +1,20 @@
 package com.dimsumlabs.door;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static android.os.PowerManager.SCREEN_BRIGHT_WAKE_LOCK;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +25,36 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        StringBuilder sb = new StringBuilder();
+        File file = new File(Environment.getExternalStorageDirectory(), "door");
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+
+            int n;
+            while ((n = fis.read(buffer)) != -1) {
+                sb.append(new String(buffer, 0, n));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         WebView view = (WebView) findViewById(R.id.activity_main);
         view.setWebViewClient(new WebViewClient());
         view.getSettings().setJavaScriptEnabled(true);
-        view.loadUrl("http://door.lan");
+        view.loadUrl(sb.toString());
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl = pm.newWakeLock(SCREEN_BRIGHT_WAKE_LOCK, "door");
+        wl.acquire();
     }
 }
